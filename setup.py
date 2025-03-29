28201cc851b9801bb7ee1f1b89ddf016a3d7463c
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2025-03-28 22:39:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'pandas'],
-    author='Your Name',
+    author='Pedro D. Pinto',
     description='Bayesian inference for coupled phase oscillators',
-    url='https://github.com/yourusername/bayesinf',
+    url='https://github.com/p3dr0id/bayesinf',
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
