25ba1e48c0797f046674cb8f501bcd497ea4a292
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2025-03-31 10:34:04 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,21 @@ setup(
     name='dynabayes',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'pandas'],
+    install_requires=[
+        "numpy>=1.26.0",
+        "matplotlib",
+        "pandas>=2.2.2"
+    ],
     author='Pedro D. Pinto',
-    description='dynamic bayesian inference for coupled phase oscillators',
+    author_email='pedrodp42@gmail.com'
+    description='Dynamic Bayesian Inference Package',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/p3dr0id/DynaBayes',
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.8',
 )
