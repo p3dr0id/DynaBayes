3ee7a75f93d0983434151e64d3d1d470765b0693
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2025-03-31 13:00:02 +0000

    Update: 2025-03-31 13:00:02

--- examples/example_2osc.py
+++ examples/example_2osc.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import bayesinf as bi
+import dynabayes as db
 
 # Example: Inference on 2 coupled phase oscillators
 
-omega = [lambda t: 2 - 0.5 * np.sin(2 * np.pi * 0.00151 * t), bi.const(4.53)]
+omega = [lambda t: 2 - 0.5 * np.sin(2 * np.pi * 0.00151 * t), db.const(4.53)]
 A = [
-    [bi.const(0.8), lambda t: 0.8 - 0.3 * np.sin(2 * np.pi * 0.0012 * t)],
-    [bi.const(0.0), bi.const(0.6)]
+    [db.const(0.8), lambda t: 0.8 - 0.3 * np.sin(2 * np.pi * 0.0012 * t)],
+    [db.const(0.0), db.const(0.6)]
 ]
 B = [
-    [bi.const(0.0), bi.const(0.0)],
-    [bi.const(0.0), bi.const(0.0)]
+    [db.const(0.0), db.const(0.0)],
+    [db.const(0.0), db.const(0.0)]
 ]
 
 # Simulate system
-phi, true_funcs, t = bi.simulate_model(omega, A, B, E=[0.03, 0.01], t_max=2000, dt=0.01)
+phi, true_funcs, t = db.simulate_model(omega, A, B, E=[0.03, 0.01], t_max=2000, dt=0.01)
 
 # Run inference
-params, centers = bi.run_inference(phi, dt=0.01, E_true=[0.03, 0.01], pw=0.2, t=t)
+params, centers = db.run_inference(phi, dt=0.01, E_true=[0.03, 0.01], pw=0.2, t=t)
 
 # Plot results
-bi.plot_parameters(params, true_funcs, centers)
+db.plot_parameters(params, true_funcs, centers)
 plt.savefig("params_inference.png")
-bi.show_summary(params, true_funcs, centers)
+db.show_summary(params, true_funcs, centers)
